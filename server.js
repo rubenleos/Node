b35bdd3357dd07a83fb281241dc1784407c03572
ruben leos sanchez <ruben_196r@hotmail.com>
2022-11-24 11:39:37 -0600

    base completa

--- server.js
+++ server.js
@@ -1,14 +1,47 @@
 const express = require('express');
-const { listen } = require('express/lib/application');
-const app =express();
+const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const logger = require('morgan');
+const cors = require('cors');
 
-//puerto que esta escuchando nuestro server 
-const port = process.env.PORT || 8000;
-app.set('port',port);
+/*
+* IMPORTAR RUTAS
+*/
 
-server-listen(8000,'172.16.1.15'|| 'localhost',function(){
-console.log('Aplicacion de node js '+ process.pid + 'iniciada...')
 
+const port = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+
+app.use(cors());
+
+app.disable('x-powered-by');
+
+app.set('port', port);
+
+/*
+* LLAMADO DE LAS RUTAS
+*/
+
+
+
+server.listen(3000, '172.16.1.15' || 'localhost', function() {
+    console.log('Aplicacion de NodeJS ' + port + ' Iniciada...')
+});
+
+
+// ERROR HANDLER
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(err.status || 500).send(err.stack);
+});
+
+app.get('/',  (req, res) => {
+    res.send('Ruta raiz del backend');
 });
